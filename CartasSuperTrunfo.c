e4e0fd9bfed241f04049f3f688cb7018b11d5a90
MrChese <gabrielmarcheseee@gmail.com>
2025-04-19 16:35:41 +0000

    adcionei a função de calculo proposta no disafio 2

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,19 +2,21 @@
 
 int main() {
     char estado; 
-    int numero;
+    float numero;
     char cidade[20];
     float area;
     float populacao;
     float pib;
     float turismo;
+    float densidade;
+    float PibPerCapito;
 
     printf(" seja bem vindo ao jogo Super Trunfo \n");
     printf("para começar digite a primeira letra do seu estado:\n ");
     scanf("%c", &estado);
 
     printf("agora digite escolha um numero para sua carta:\n");
-    scanf("%i", &numero);
+    scanf("%f", &numero);
 
     printf("digite o nome da sua cidade: \n");
     scanf("%s", &cidade);
@@ -31,11 +33,15 @@ int main() {
     printf("para finalizar digite o numero de pontos turisticos da sua cidade:\n");
     scanf("%f", &turismo);
 
+    PibPerCapito = (pib / populacao);
+
+    densidade = (populacao / area);
+
     printf(" no final sua carta ficou:\n");
     
     printf("Letra que representa o seu estado: %c \n", estado);
 
-    printf("codigo da carta: %i \n", numero);
+    printf("codigo da carta: %c %0.1f \n", estado ,numero);
 
     printf("cidade: %s \n", cidade);
 
@@ -45,7 +51,11 @@ int main() {
 
     printf("PIB: %f \n", pib);
 
-    printf("Pontos turisticos %f \n", turismo);
+    printf("Pontos turisticos: %f \n", turismo);
+
+    printf("o Pib Per Capito é igual a: %f \n", PibPerCapito);
+
+    printf("a densidade populacional é de %f hab/km²: \n", densidade);
 
     return 0;
 }
