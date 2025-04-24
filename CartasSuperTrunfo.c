f6b1bae05471ee4aa424a29170c229540b721bea
MrChese <gabrielmarcheseee@gmail.com>
2025-04-24 21:32:27 +0000

    Adcionei o pção de escolher mais de um atributo pra decidir a carta vencedora

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -12,19 +12,19 @@ int main() {
     float pibPerCapito, pibPerCapito2;
     long double resultado, resultado2;
     float densidadeInvertida, densidadeInvertida2;
-    int opcao;
+    int opcao, opcao2;
 
     printf(" seja bem vindo ao jogo Super Trunfo \n");
     printf("para começar digite a primeira letra do seu estado:\n ");
     scanf("%c", &estado);
 
-    printf("agora digite escolha um numero para sua carta:\n");
+    printf("agora escolha um numero para sua carta:\n");
     scanf("%f", &numero);
 
     printf("digite o nome da sua cidade: \n");
     scanf("%s", &cidade);
 
-    printf("digite quantos km sua cidade tem:\n");
+    printf("digite quantos km² sua cidade tem:\n");
     scanf("%f", &area);
 
     printf("digite o numero da população da sua cidade:\n");
@@ -38,11 +38,12 @@ int main() {
 
     pibPerCapito = (pib / (float)populacao);
     
-    densidade = (populacao / area);
+    densidade = ((float)populacao / area);
     
     densidadeInvertida = (1 / densidade);
     
-    resultado = ( populacao + area + pib + densidadeInvertida + turismo + pibPerCapito );
+    resultado = (populacao / 1000.0) + (area / 100.0) +(pib / 1000000.0) + densidadeInvertida + turismo + (pibPerCapito / 1000.0);
+
 
 
     printf(" no final sua carta ficou:\n");
@@ -53,9 +54,9 @@ int main() {
 
     printf("cidade: %s \n", cidade);
 
-    printf("area: %f \n", area);
+    printf("área: %f \n", area);
 
-    printf("populacao: %d \n", populacao);
+    printf("população: %d \n", populacao);
 
     printf("PIB: %0.2f \n", pib);
 
@@ -71,13 +72,13 @@ int main() {
     printf("para começar digite a primeira letra do seu estado:\n ");
     scanf(" %c", &estado2);
 
-    printf("agora digite escolha um numero para sua carta:\n");
+    printf("agora escolha um numero para sua carta:\n");
     scanf("%f", &numero2);
 
     printf("digite o nome da sua cidade: \n");
     scanf("%s", &cidade2);
 
-    printf("digite quantos km sua cidade tem:\n");
+    printf("digite quantos km² sua cidade tem:\n");
     scanf("%f", &area2);
 
     printf("digite o numero da população da sua cidade:\n");
@@ -91,11 +92,11 @@ int main() {
 
     pibPerCapito2 = (pib2 / (float)populacao2);
     
-    densidade2 = (populacao2 / area2);
+    densidade2 = ((float)populacao2 / area2);
     
     densidadeInvertida2 = (1 / densidade2);
     
-    resultado2 = ( populacao2 + area2 + pib2 + densidadeInvertida2 + turismo2 + pibPerCapito2 );
+    resultado2 = (populacao2 / 1000.0) + (area2 / 100.0) +(pib2 / 1000000.0) + densidadeInvertida2 + turismo2 + (pibPerCapito2 / 1000.0);
 
 
     printf(" no final sua carta ficou:\n");
@@ -106,9 +107,9 @@ int main() {
 
     printf("cidade: %s \n", cidade2);
 
-    printf("area: %f \n", area2);
+    printf("área: %f \n", area2);
 
-    printf("populacao: %d \n", populacao2);
+    printf("população: %d \n", populacao2);
 
     printf("PIB: %0.2f \n", pib2);
 
@@ -121,7 +122,7 @@ int main() {
     printf("o super poder é de %ld : \n", resultado2);
 
     
-    printf("Agora você pode escolher um dos atributos da carta para comparar e escolher o vencedor:\n");
+    printf("Agora você pode escolher dois atributos da carta para comparar e escolher o vencedor:\n");
     printf("1. Antes de começar, que tal ler as regras?\n");
     printf("2. População\n");
     printf("3. Área\n");
@@ -138,54 +139,293 @@ int main() {
             break;
 
         case 2:
-            printf("O atributo escolhido foi a população.\n");
-            if (populacao > populacao2) {
-                printf("A carta vencedora é %s.\n", cidade);
-            } else {
-                printf("A carta vencedora é %s.\n", cidade2);
+            printf("O primeiro atributo escolhido foi a População.\n");
+
+            printf(" Escolha a segunda opção:\n");
+            printf("1. População\n");
+            printf("2. Área\n");
+            printf("3. PIB\n");
+            printf("4. Número de pontos turísticos\n");
+            printf("5. Densidade demográfica\n");
+
+            scanf("%d", &opcao2);
+
+            switch (opcao2) {
+
+                case 1:
+                
+                printf(" A segunda opção escolida foi a População\n ");
+
+                populacao > populacao2 ? printf(" A carta vencedora é %s. \n ", cidade) 
+            : printf(" A carta vencedora é %s. \n", cidade2);
+            break;
+
+            case 2:
+
+            printf(" A segunda opção escolida foi a Àrea\n ");
+
+                populacao > area2 ? printf(" A carta vencedora é %s. \n ", cidade) 
+            : printf(" A carta vencedora é %s. \n", cidade2);
+            break;
+
+            case 3:
+                
+                printf(" A segunda opção escolida foi o Pib\n ");
+
+                populacao > pib2 ? printf(" A carta vencedora é %s. \n ", cidade) 
+            : printf(" A carta vencedora é %s. \n", cidade2);
+            break;
+
+            case 4:
+
+            printf(" A segunda opção escolida foi os pontos Turísticos\n ");
+
+                populacao > turismo2 ? printf(" A carta vencedora é %s. \n ", cidade) 
+            : printf(" A carta vencedora é %s. \n", cidade2);
+            break;
+
+            case 5:
+
+            printf(" A segunda opção escolida foi a Densidade demográfica\n ");
+
+                populacao > densidade2 ? printf(" A carta vencedora é %s. \n ", cidade) 
+            : printf(" A carta vencedora é %s. \n", cidade2);
+            break;
+
             }
             break;
 
         case 3:
-            printf("O atributo escolhido foi a área.\n");
-            if (area > area2) {
-                printf("A carta vencedora é %s.\n", cidade);
-            } else {
-                printf("A carta vencedora é %s.\n", cidade2);
-            }
+        printf("O primeiro atributo escolhido foi a Àrea.\n");
+
+        printf(" Escolha a segunda opção:\n");
+            printf("1. População\n");
+            printf("2. Área\n");
+            printf("3. PIB\n");
+            printf("4. Número de pontos turísticos\n");
+            printf("5. Densidade demográfica\n");
+
+            scanf("%d", &opcao2);
+
+            switch (opcao2) {
+
+                case 1:
+                
+                printf(" A segunda opção escolida foi a População\n ");
+
+                area > populacao2 ? printf(" A carta vencedora é %s. \n ", cidade) 
+            : printf(" A carta vencedora é %s. \n", cidade2);
+            break;
+
+            case 2:
+
+            printf(" A segunda opção escolida foi a Àrea\n ");
+
+                area > area2 ? printf(" A carta vencedora é %s. \n ", cidade) 
+            : printf(" A carta vencedora é %s. \n", cidade2);
+            break;
+
+            case 3:
+                
+                printf(" A segunda opção escolida foi o Pib\n ");
+
+                area > pib2 ? printf(" A carta vencedora é %s. \n ", cidade) 
+            : printf(" A carta vencedora é %s. \n", cidade2);
+            break;
+
+            case 4:
+
+            printf(" A segunda opção escolida foi os pontos Turísticos\n ");
+
+                area > turismo2 ? printf(" A carta vencedora é %s. \n ", cidade) 
+            : printf(" A carta vencedora é %s. \n", cidade2);
+            break;
+
+            case 5:
+
+            printf(" A segunda opção escolida foi a Densidade demográfica\n ");
+
+                area > densidade2 ? printf(" A carta vencedora é %s. \n ", cidade) 
+            : printf(" A carta vencedora é %s. \n", cidade2);
+            break;
+            
+        }
             break;
 
         case 4:
-            printf("O atributo escolhido foi o PIB.\n");
-            if (pib > pib2) {
-                printf("A carta vencedora é %s.\n", cidade);
-            } else {
-                printf("A carta vencedora é %s.\n", cidade2);
-            }
-            break;
+        printf("O primeiro atributo escolhido foi o PIB.\n");
+
+        printf(" Escolha a segunda opção:\n");
+        printf("1. População\n");
+        printf("2. Área\n");
+        printf("3. PIB\n");
+        printf("4. Número de pontos turísticos\n");
+        printf("5. Densidade demográfica\n");
+
+        scanf("%d", &opcao2);
+
+        switch (opcao2) {
+
+            case 1:
+            
+            printf(" A segunda opção escolida foi a População\n ");
+
+            pib > populacao2 ? printf(" A carta vencedora é %s. \n ", cidade) 
+        : printf(" A carta vencedora é %s. \n", cidade2);
+        break;
+
+        case 2:
+
+        printf(" A segunda opção escolida foi a Àrea\n ");
+
+            pib > area2 ? printf(" A carta vencedora é %s. \n ", cidade) 
+        : printf(" A carta vencedora é %s. \n", cidade2);
+        break;
+
+        case 3:
+            
+            printf(" A segunda opção escolida foi o Pib\n ");
+
+            pib > pib2 ? printf(" A carta vencedora é %s. \n ", cidade) 
+        : printf(" A carta vencedora é %s. \n", cidade2);
+        break;
+
+        case 4:
+
+        printf(" A segunda opção escolida foi os pontos Turísticos\n ");
+
+            pib > turismo2 ? printf(" A carta vencedora é %s. \n ", cidade) 
+        : printf(" A carta vencedora é %s. \n", cidade2);
+        break;
 
         case 5:
-            printf("O atributo escolhido foi o número de pontos turísticos.\n");
-            if (turismo > turismo2) {
-                printf("A carta vencedora é %s.\n", cidade);
-            } else {
-                printf("A carta vencedora é %s.\n", cidade2);
-            }
+
+        printf(" A segunda opção escolida foi a Densidade demográfica\n ");
+
+            pib > densidade2 ? printf(" A carta vencedora é %s. \n ", cidade) 
+        : printf(" A carta vencedora é %s. \n", cidade2);
+        break;
+    
+    }
+        break;
+
+        case 5:
+        printf("O primeiro atributo escolhido foi o Numero de pontos Turísticos.\n");
+
+        printf(" Escolha a segunda opção:\n");
+            printf("1. População\n");
+            printf("2. Área\n");
+            printf("3. PIB\n");
+            printf("4. Número de pontos turísticos\n");
+            printf("5. Densidade demográfica\n");
+
+            scanf("%d", &opcao2);
+
+            switch (opcao2) {
+
+                case 1:
+                
+                printf(" A segunda opção escolida foi a População\n ");
+
+                turismo > populacao2 ? printf(" A carta vencedora é %s. \n ", cidade) 
+            : printf(" A carta vencedora é %s. \n", cidade2);
+            break;
+
+            case 2:
+
+            printf(" A segunda opção escolida foi a Àrea\n ");
+
+                turismo > area2 ? printf(" A carta vencedora é %s. \n ", cidade) 
+            : printf(" A carta vencedora é %s. \n", cidade2);
+            break;
+
+            case 3:
+                
+                printf(" A segunda opção escolida foi o Pib\n ");
+
+                turismo > pib2 ? printf(" A carta vencedora é %s. \n ", cidade) 
+            : printf(" A carta vencedora é %s. \n", cidade2);
+            break;
+
+            case 4:
+
+            printf(" A segunda opção escolida foi os pontos Turísticos\n ");
+
+                turismo > turismo2 ? printf(" A carta vencedora é %s. \n ", cidade) 
+            : printf(" A carta vencedora é %s. \n", cidade2);
+            break;
+
+            case 5:
+
+            printf(" A segunda opção escolida foi a Densidade demográfica\n ");
+
+                turismo > densidade2 ? printf(" A carta vencedora é %s. \n ", cidade) 
+            : printf(" A carta vencedora é %s. \n", cidade2);
+            break;
+           }
             break;
 
         case 6:
-            printf("O atributo escolhido foi a densidade demográfica.\n");
-            if (densidade < densidade2) {
-                printf("A carta vencedora é %s.\n", cidade);
-            } else {
-                printf("A carta vencedora é %s.\n", cidade2);
+        printf("O primeiro atributo escolhido foi a Densidade demográfica.\n");
+
+        printf(" Escolha a segunda opção:\n");
+            printf("1. População\n");
+            printf("2. Área\n");
+            printf("3. PIB\n");
+            printf("4. Número de pontos turísticos\n");
+            printf("5. Densidade demográfica\n");
+
+            scanf("%d", &opcao2);
+
+            switch (opcao2) {
+
+                case 1:
+                
+                printf(" A segunda opção escolida foi a População\n ");
+
+                densidade > populacao2 ? printf(" A carta vencedora é %s. \n ", cidade) 
+            : printf(" A carta vencedora é %s. \n", cidade2);
+            break;
+
+            case 2:
+
+            printf(" A segunda opção escolida foi a Àrea\n ");
+
+                densidade > area2 ? printf(" A carta vencedora é %s. \n ", cidade) 
+            : printf(" A carta vencedora é %s. \n", cidade2);
+            break;
+
+            case 3:
+                
+                printf(" A segunda opção escolida foi o Pib\n ");
+
+                densidade > pib2 ? printf(" A carta vencedora é %s. \n ", cidade) 
+            : printf(" A carta vencedora é %s. \n", cidade2);
+            break;
+
+            case 4:
+
+            printf(" A segunda opção escolida foi os pontos Turísticos\n ");
+
+                densidade > turismo2 ? printf(" A carta vencedora é %s. \n ", cidade) 
+            : printf(" A carta vencedora é %s. \n", cidade2);
+            break;
+
+            case 5:
+
+            printf(" A segunda opção escolida foi a Densidade demográfica\n ");
+
+                densidade > densidade2 ? printf(" A carta vencedora é %s. \n ", cidade) 
+            : printf(" A carta vencedora é %s. \n", cidade2);
+            break;
+
             }
             break;
 
         default:
             printf("A opção não é válida.\n");
             break;
-    }
+    } 
 
     return 0;
 }
